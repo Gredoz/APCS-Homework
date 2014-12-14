ab2db666ca11550ffb1652679d735f832a22eb9b
Gredoz <stuyredoz@gmail.com>
2014-12-14 11:57:27 -0500

    basics done

---
+++ HW22-Searching/Searching.java
@@ -0,0 +1,26 @@
+public class Searching{
+   
+    Comparable[] a;
+    private int len = 0;
+
+
+
+    public void additem(Comparable val){
+	if (size < a.length){
+	    data
+    
+
+
+
+
+
+
+
+
+    //*******DRIVER*******
+    public static void main(String[] args){
+	int a = 9;
+	int b = 10;
+	System.out.println(a+b);
+    }
+}
